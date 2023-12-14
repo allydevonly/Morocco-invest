7afdfc3cc4a638731780a331b283721b39af6bca
zakariae bouadel <zakariaebouadel@gmail.com>
2023-12-14 20:49:33 +0100

    dakchi

--- indexapp.js
+++ indexapp.js
@@ -26,43 +26,49 @@ nextBtn.addEventListener('click', () => {
 showMessage();
 
 
-function showDescription(selection) {
-  if (selection === 'Tourism') {
-    document.getElementById('description').innerText = 'Description for Tourism...';
-    document.getElementById('category1').style.display = 'none';
-    document.getElementById('category2').style.display = 'block';
-  } else if (selection === 'Healthcare') {
-    document.getElementById('description').innerText = 'Description for Healthcare and Pharmaceuticals...';
-    document.getElementById('category1').style.display = 'none';
-    document.getElementById('category2').style.display = 'block';
-  } else if (selection === 'Agriculture') {
-    document.getElementById('description').innerText = 'Description for Agriculture and Agribusiness...';
-    document.getElementById('category1').style.display = 'none';
-    document.getElementById('category2').style.display = 'block';
-  } else if (selection === 'High') {
-    document.getElementById('description2').innerText = 'Description for High Risk...';
-    document.getElementById('category2').style.display = 'none';
-    document.getElementById('category3').style.display = 'block';
-  } else if (selection === 'Low') {
-    document.getElementById('description2').innerText = 'Description for Low Risk...';
-    document.getElementById('category2').style.display = 'none';
-    document.getElementById('category3').style.display = 'block';
-  } else if (selection === 'Medium') {
-    document.getElementById('description2').innerText = 'Description for Medium Risk/Moderate Risk...';
-    document.getElementById('category2').style.display = 'none';
-    document.getElementById('category3').style.display = 'block';
-  } else if (selection === '100k-500k') {
-    document.getElementById('description3').innerText = 'Description for $100,000 to $500,000 budget...';
-    document.getElementById('category3').style.display = 'none';
-    // Do something with the final selection
-  } else if (selection === '500k-800k') {
-    document.getElementById('description3').innerText = 'Description for $500,000 to $800,000 budget...';
-    document.getElementById('category3').style.display = 'none';
-    // Do something with the final selection
-  } else if (selection === '800k-1m') {
-    document.getElementById('description3').innerText = 'Description for $800,000 to $1 million budget...';
-    document.getElementById('category3').style.display = 'none';
-    // Do something with the final selection
-  }
+if(true){
+
+
 }
+
+
+// function showDescription(selection) {
+//   if (selection === 'Tourism') {
+//     document.getElementById('description').innerText = 'Description for Tourism...';
+//     document.getElementById('category1').style.display = 'none';
+//     document.getElementById('category2').style.display = 'block';
+//   } else if (selection === 'Healthcare') {
+//     document.getElementById('description').innerText = 'Description for Healthcare and Pharmaceuticals...';
+//     document.getElementById('category1').style.display = 'none';
+//     document.getElementById('category2').style.display = 'block';
+//   } else if (selection === 'Agriculture') {
+//     document.getElementById('description').innerText = 'Description for Agriculture and Agribusiness...';
+//     document.getElementById('category1').style.display = 'none';
+//     document.getElementById('category2').style.display = 'block';
+//   } else if (selection === 'High') {
+//     document.getElementById('description2').innerText = 'Description for High Risk...';
+//     document.getElementById('category2').style.display = 'none';
+//     document.getElementById('category3').style.display = 'block';
+//   } else if (selection === 'Low') {
+//     document.getElementById('description2').innerText = 'Description for Low Risk...';
+//     document.getElementById('category2').style.display = 'none';
+//     document.getElementById('category3').style.display = 'block';
+//   } else if (selection === 'Medium') {
+//     document.getElementById('description2').innerText = 'Description for Medium Risk/Moderate Risk...';
+//     document.getElementById('category2').style.display = 'none';
+//     document.getElementById('category3').style.display = 'block';
+//   } else if (selection === '100k-500k') {
+//     document.getElementById('description3').innerText = 'Description for $100,000 to $500,000 budget...';
+//     document.getElementById('category3').style.display = 'none';
+//     // Do something with the final selection
+//   } else if (selection === '500k-800k') {
+//     document.getElementById('description3').innerText = 'Description for $500,000 to $800,000 budget...';
+//     document.getElementById('category3').style.display = 'none';
+//     // Do something with the final selection
+//   } else if (selection === '800k-1m') {
+//     document.getElementById('description3').innerText = 'Description for $800,000 to $1 million budget...';
+//     document.getElementById('category3').style.display = 'none';
+//     // Do something with the final selection
+//   }
+// }
 
